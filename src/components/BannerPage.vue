e8d7530d3db8df7aa8f2ccadf9e6c4b48aac997e
test-user <candysweet1098@gmail.com>
2024-01-16 23:00:04 +0800

    測試提交1

--- src/components/BannerPage.vue
+++ src/components/BannerPage.vue
@@ -6,7 +6,7 @@
         <div class="banner">
           <img src="@/assets/bannerPic.jfif" alt="">
           <div class="float">
-            <h1>白頭翁 (Chinese bulbul)</h1>
+            <h1>白頭翁 (Chinese bulbul)測試測試</h1>
             <p>又名白頭鵯。以果實、昆蟲為主食，無法消化小米、穀類。平均壽命約 8~10 年。</p>
           </div>
       </div>
