b37e8d19bcac1924dee13ab711b5689e52af1cc8
test-user <candysweet1098@gmail.com>
2024-01-31 22:52:10 +0800

    解決合併衝突

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@ const onHambuger = () => {
       <!-- 漢堡選單 -->
       <div class="sec-menu hidden-sm-and-up">
         <el-button :icon="Expand" size="large" @click="onHambuger"></el-button>
-        <h2>白頭翁不吃小米要吃大米</h2>
+        <h2>白頭翁不吃小米要吃大米要吃大米</h2>
         <h3>老鼠愛大米</h3>
         <div class="logoTitle">
           <svg viewBox="0 0 100 100">
